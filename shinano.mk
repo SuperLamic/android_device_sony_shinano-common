e21e922f4239120c09579a52d759e57ffd92680d
Ondrej Hlavac <hlavaco@gmail.com>
2017-06-23 17:37:07 +0200

    shinano-common: init: Add back camera init

--- shinano.mk
+++ shinano.mk
@@ -40,6 +40,7 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(COMMON_PATH)/rootdir/init.camera.rc:root/init.camera.rc \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
